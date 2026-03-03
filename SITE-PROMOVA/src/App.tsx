7f45c5920bbae36a2a0a6b2eca71ec548bedd691
Anathyon <anathyon@hotmail.com>
2026-03-03 15:13:40 -0300

    Primeiro commit site promova

---
+++ SITE-PROMOVA/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+        <h1 className="text-3xl font-bold underline bg-blue-500">Promova</h1>
+        <p className="text-xl">Promova</p>
+    </>
+  )
+}
+
+export default App
